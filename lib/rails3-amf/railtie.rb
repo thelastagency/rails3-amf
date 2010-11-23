37d6fa668cd60ef84f7d5da1398f5039013eb61b
mfoster <fosterinfo@gmail.com>
2010-11-22 22:16:28 -0800

    Core modifications to enable loading legacy mappings.

--- lib/rails3-amf/railtie.rb
+++ lib/rails3-amf/railtie.rb
@@ -1,6 +1,8 @@
 require 'rocketamf'
 require 'rails'
 
+require 'rubyamf/configuration' #fosrias
+
 require 'rails3-amf/serialization'
 require 'rails3-amf/action_controller'
 require 'rails3-amf/configuration'
@@ -10,6 +12,11 @@ require 'rails3-amf/request_processor'
 module Rails3AMF
   class Railtie < Rails::Railtie
     config.rails3amf = Rails3AMF::Configuration.new
+
+    #fosrias RubyAMF mapping accesses models, so we load the mapping after the application initializes
+    config.after_initialize do
+      load File.expand_path(::Rails.root.to_s) + '/config/rubyamf_config.rb'
+    end
 
     initializer "rails3amf.middleware" do
       config.app_middleware.use Rails3AMF::RequestParser, config.rails3amf, Rails.logger
