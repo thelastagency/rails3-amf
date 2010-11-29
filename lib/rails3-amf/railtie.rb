d06034a75a9fdeebc589efca78b2ea37bc85e43d
mfoster <fosterinfo@gmail.com>
2010-11-29 12:30:11 -0800

    Updated railtie based on other refactorings.

--- lib/rails3-amf/railtie.rb
+++ lib/rails3-amf/railtie.rb
@@ -1,15 +1,17 @@
 require 'rocketamf'
 require 'rails'
 
-require 'rails3-amf/serialization'
+#require 'rails3-amf/serialization' fosrias: See rubyamf/serialization
 require 'rails3-amf/action_controller'
 require 'rails3-amf/configuration'
 require 'rails3-amf/request_parser'
 require 'rails3-amf/request_processor'
 
-require 'rubyamf/rails3/rails3_serialization' #In this order to override default serialization
-require 'rubyamf/rails3/rails3_configuration'
-require 'rubyamf/rails3/fault_object'
+require 'rubyamf/util/string'
+require 'rubyamf/extensions/fault_object'
+require 'rubyamf/extensions/rocketamf'
+require 'rubyamf/extensions/configuration'
+require 'rubyamf/serialization'
 
 module Rails3AMF
   class Railtie < Rails::Railtie
@@ -24,14 +26,6 @@ module Rails3AMF
     # autoloading works.
     config.after_initialize do
       load File.expand_path(::Rails.root.to_s) + '/config/rubyamf_config.rb'
-
-      # If we use array collections, we override how arrays are written.
-      require 'rubyamf/array_collection_serializer' if RubyAMF::Configuration::ClassMappings.use_array_collection
-
-      #Map FaultObject for Rails 3 which extends RocketAMF::Values::ErrorMessage
-      RocketAMF::ClassMapper.define do |m|
-        m.map :as => 'flex.messaging.messages.ErrorMessage', :ruby => 'FaultObject'
-      end
     end
   end
 end
